4ce1b462030f29298e4ec56c217479106c4ab25f
SeeChen Lee <leeseechen@gmail.com>
2021-11-28 02:07:32 +0800

    Create timeConvert.java

---
+++ src/com/MediaPlayer/Controller/timeConvert.java
@@ -0,0 +1,19 @@
+package com.MediaPlayer.Controller;
+
+public class timeConvert {
+
+    private String lessThanTen(int num){
+        String time = "";
+        if(num < 10)
+            time = "0" + String.valueOf(num);
+        else
+            time = String.valueOf(num);
+        return time;
+    }
+
+    public String secondToTime(int toCovert){
+        String time = "";
+        time = lessThanTen(toCovert);
+        return time;
+    }
+}
